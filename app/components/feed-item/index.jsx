e87958247cb355a5be25db6cbb779e9b7873fc75
Tsj <thibault.ingargiola@outlook.fr>
2017-12-15 11:51:15 +0100

    display aside feed-item.

--- app/components/feed-item/index.jsx
+++ app/components/feed-item/index.jsx
@@ -1,28 +1,44 @@
 import * as React from "React";
 import "./style.scss";
-
+import { navigate } from "../../helpers/navigation-helper";
 
 export class FeedItem extends React.Component {
   constructor(props) {
     super(props);
+    this._renderImg = this._renderImg.bind(this);
+    this._navigateToTwitter = this._navigateToTwitter.bind(this);
   }
 
   render() {
     return (
       <li data-component="feed-item">
         <div className="aside">
+          {this._renderImg()}
+          <div className={`dot ${!this.props.read ? "new" : ""}`}/>
         </div>
         <div className="content">
-          <div className="content-head">
+            <div className="content-head">
+            </div>
+            <div className="content-name">
+            </div>
+            <div className="content-message">
+            </div>
           </div>
-          <div className="content-name">
-          </div>
-          <div className="content-message">
-          </div>
-        </div>
       </li>
-    );
+        );
   }
-}
+
+  _renderImg() {
+    return (
+      <div className="img-handler">
+          <img src={this.props.picture_url} />
+          <img className="twitter" src="http://icons.iconarchive.com/icons/sicons/basic-round-social/512/twitter-icon.png" onClick={this._navigateToTwitter} />
+        </div>);
+  }
+
+  _navigateToTwitter() {
+          navigate(this.props.source_url)
+        }
+        }
 
 
